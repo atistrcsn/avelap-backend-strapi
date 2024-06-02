f06d1a71730a17e3170a1c99c352fb6cf953a26c
atistrcsn <atistrcsn@users.noreply.github.com>
2024-06-02 15:36:43 +0200

    add database config

---
+++ config/env/database.ts
@@ -0,0 +1,20 @@
+import { parse } from "pg-connection-string";
+export default ({ env }) => {
+  const config = parse(process.env.DATABASE_URL);
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      debug: false,
+    },
+  };
+};
