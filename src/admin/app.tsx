7cadfc972a7cd9563f5e20c88f7b09c63b1596fb
atistrcsn <atistrcsn@users.noreply.github.com>
2024-08-18 13:52:01 +0200

    test hu translation

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,6 +1,11 @@
 export default {
   config: {
     locales: ["hu", "en"],
+    translations: {
+      hu: {
+        "Settings.application.customization.menu-logo.carousel-hint":"Cserélje le a logót a navigáció felett"
+      },
+    },
   },
   bootstrap(app) {
     console.log(app);
